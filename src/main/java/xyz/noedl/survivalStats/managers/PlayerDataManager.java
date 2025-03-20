d36d217cf1ebe23a09630303e0fdb4646fb4c2c5
NoedL <88205753+N0edL@users.noreply.github.com>
2025-03-20 17:33:51 +0100

    Added loading of player data

--- src/main/java/xyz/noedl/survivalStats/managers/PlayerDataManager.java
+++ src/main/java/xyz/noedl/survivalStats/managers/PlayerDataManager.java
@@ -1,29 +1,47 @@
 package xyz.noedl.survivalStats.managers;
 
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 import xyz.noedl.survivalStats.utils.PlayerData;
 
 import org.bukkit.entity.Player;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerDataManager {
 
-    private final Map<Player, PlayerData> playerDataMap = new HashMap<>();
+    private final JavaPlugin plugin;
+    private final File dataFile;
+    private final Map<String, PlayerData> playerDataMap;
 
-    public PlayerData getPlayerData(Player player) {
-        if (!playerDataMap.containsKey(player)) {
-            playerDataMap.put(player, new PlayerData());
-        }
-        return playerDataMap.get(player);
+    public PlayerDataManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+        this.dataFile = new File(plugin.getDataFolder(), "player_data.yml");
+        this.playerDataMap = new HashMap<>();
+        loadPlayerData();
     }
 
-    public void updateSurvivalDays(Player player) {
-        PlayerData playerData = getPlayerData(player);
-        playerData.incrementDaysSurvived();
-        savePlayerData(player);
+    public void loadPlayerData(){
+        if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException e) {
+                plugin.getLogger().warning("Error creating data(player_data.yml) file");
+                return;
+            }
+        }
+
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+
+        for (String playerName : config.getKeys(false)) {
+            int daysSurvived = config.getInt(playerName + ".daysSurvived");
+            int deathCount = config.getInt(playerName + ".deathCount");
+            PlayerData playerData = new PlayerData(daysSurvived, deathCount);
+            playerDataMap.put(playerName, playerData);
+        }
     }
 
     public void savePlayerData(Player player) {
@@ -38,7 +56,17 @@ public class PlayerDataManager {
         try {
             config.save(dataFile);
         } catch (IOException e) {
-            plugin.getLogger.warning("Error saving player data!");
+            plugin.getLogger().warning("Error saving player data!");
         }
+    }
+
+    public PlayerData getPlayerData(Player player) {
+        return playerDataMap.computeIfAbsent(player.getName(), name -> new PlayerData());
+    }
+
+    public void updateSurvivalDays(Player player) {
+        PlayerData playerData = getPlayerData(player);
+        playerData.incrementDaysSurvived();
+        savePlayerData(player);
     }
 }
