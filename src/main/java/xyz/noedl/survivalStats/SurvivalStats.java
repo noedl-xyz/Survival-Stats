10c4a9b70741a6f97b4df685b7416347188181e6
NoedL <88205753+N0edL@users.noreply.github.com>
2025-03-20 15:35:40 +0100

    Initial Commit

---
+++ src/main/java/xyz/noedl/survivalStats/SurvivalStats.java
@@ -0,0 +1,17 @@
+package xyz.noedl.survivalStats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
